8bb905bcc964791aa7857c9d6a467f264c5f7376
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-15 19:50:46 -0700

    added utils, quest pages, map pages

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-import { USER } from './storage/storage-utils.js';
+import { saveUser } from './storage/storage-utils.js';
 
 const form = document.querySelector('form');
 
+//loads on submit
 form.addEventListener('submit', (e) => {
     e.preventDefault;
 
     const data = new FormData(form);
 
-    //Set Up State Object
-    const user = {
+    //Set Up State Object & save to local storage (refactored)
+    saveUser({
         name: data.get('name'),
         class: data.get('class'),
         gold: 0,
         hp: 50,
         completed: {}
-    };
-
-    //Set State Object in Local Storage
-    const stringyUser = JSON.stringify(user);
-    localStorage.setItem(USER, stringyUser);
+    });
+    //redirect to map page
+    window.location.href = './map/index.html';
 });
 
 
--- storage/storage-utils.js
+++ storage/storage-utils.js
@@ -1,7 +1,13 @@
 
 export const USER = 'USER';
 
+//Initialize State Object in Local Storage
 export function saveUser(user) {
     const stringyUser = JSON.stringify(user);
-    localStorage.setItem()
+    localStorage.setItem(USER, stringyUser);
+}
+
+export function getUser() {
+    const stringyUser = localStorage.getItem(user);
+    return JSON.parse(stringyUser);
 }
